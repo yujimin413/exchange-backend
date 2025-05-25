59f7e763b5b81d76821fa060715d23301c231178
yujimin413 <84549242+yujimin413@users.noreply.github.com>
2025-05-26 02:37:10 +0900

    [FEAT] 게시글 리스트 조회, 게시글 정보 조회 api 완료

--- shinhodeung/src/main/java/ShinHoDeung/demo/repository/PostRepository.java
+++ shinhodeung/src/main/java/ShinHoDeung/demo/repository/PostRepository.java
@@ -1,11 +1,12 @@
 package ShinHoDeung.demo.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import ShinHoDeung.demo.domain.Post;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository
-public interface PostRepository extends JpaRepository<Post, Integer>{
-    
+public interface PostRepository extends JpaRepository<Post, Integer> {
+    @NotNull
+    Page<Post> findByCategory(@NotNull String category, @NotNull Pageable pageable);
 }
